eb2e5779420e57cea083212dc311088a3790d6ca
Bruno A. Rolim <brolim@brolim-laptop.(none)>
2011-02-18 16:12:07 -0200

    adicionando os arquivos pro commit inicial do site da uana

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Site::Application.config.secret_token = 'b9da8fd4155e939d9b363519fa878e10894926d552c33304154177b5c1a2f575ae4d4afcc676669570b1d835f7c26ea52398db8939691b9f236e978c122ef6ee'
